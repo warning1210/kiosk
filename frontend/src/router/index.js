1777201bd8340d57427cd16332b36904e84c3118
zxcvbmn790-prog <zxcvbmn790@gmail.com>
2026-07-13 14:57:59 +0900

    Revert "test/cart_order_API_merge에서 지점 대시보드/주문관리 기능 이식"
    This reverts commit be0c266c524bc036c51ec1aecc5a141bb4367748.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,7 +3,6 @@ import HomeView from '../views/HomeView.vue'
 import OrderView from '../views/kiosk/OrderView.vue'
 import PaymentScanView from '../views/kiosk/PaymentScanView.vue'
 import AdminLoginView from '../views/admin/AdminLoginView.vue'
-import BranchDashboardView from '../views/admin/BranchDashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -11,8 +10,7 @@ const router = createRouter({
     { path: '/', name: 'home', component: HomeView },
     { path: '/kiosk/order', name: 'kiosk-order', component: OrderView },
     { path: '/pay/:token', name: 'payment-scan', component: PaymentScanView },
-    { path: '/admin/login', name: 'admin-login', component: AdminLoginView },
-    { path: '/branch/:branchId/dashboard', name: 'branch-dashboard', component: BranchDashboardView }
+    { path: '/admin/login', name: 'admin-login', component: AdminLoginView }
   ]
 })
 
