5ecc8b777376fd9e42f2bcc4dda1d5f3c80d427f
AKED2134 <dldyddnr2003@gmail.com>
2026-07-22 01:47:30 +0900

    지점용 상품 관리 노출 기능 추가

--- backend/src/main/java/com/kiosk/domain/product/BranchProductRepository.java
+++ backend/src/main/java/com/kiosk/domain/product/BranchProductRepository.java
@@ -2,5 +2,10 @@ package com.kiosk.domain.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BranchProductRepository extends JpaRepository<BranchProduct, Long> {
+    List<BranchProduct> findByBranch_BranchId(Long branchId);
+    Optional<BranchProduct> findByBranch_BranchIdAndProduct_ProductId(Long branchId, Long productId);
 }
